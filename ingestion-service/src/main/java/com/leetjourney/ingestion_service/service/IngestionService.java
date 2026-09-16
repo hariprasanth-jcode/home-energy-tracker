2e2822e5056227fffd46dd0037ba73d06b2fb65a
hariprasanth-jcode <hari.mba000@gmail.com>
2026-09-17 00:43:08 +0530

    ingestion-service

---
+++ ingestion-service/src/main/java/com/leetjourney/ingestion_service/service/IngestionService.java
@@ -0,0 +1,30 @@
+package com.leetjourney.ingestion_service.service;
+
+import com.leetjourney.ingestion_service.dto.EnergyUsageDto;
+import com.leetjourney.ingestion_service.event.EnergyUsageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class IngestionService {
+
+    private final KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate;
+
+    public IngestionService(KafkaTemplate<String,EnergyUsageEvent> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void ingestEnergyUsage(EnergyUsageDto input){
+
+        EnergyUsageEvent event = EnergyUsageEvent.builder()
+                .deviceId(input.deviceId())
+                .energyConsumed(input.energyConsumed())
+                .timestamp(input.timestamp())
+                .build();
+
+        kafkaTemplate.send("energy_usage_event", event);
+        log.info("Ingested Energy Usage Event : {} ",event);
+    }
+}
